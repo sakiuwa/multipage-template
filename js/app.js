a3ee34d360bd67e3bf5bb8bf98105220c13d3e30
Saki <uwagawas@uw.edu>
2015-11-10 09:44:42 -0800

    Update 11/10/15

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
 // Create app
-
+var myApp = angular.module('myApp', ['ui.router'])
 // Configure app
-
-// Landing page controller: define $scope.number as a number
-
-
-// About page controller: define $scope.about as a string
-
-
-// Content controller: define $scope.url as an image
-
-
+myApp.config(function($stateProvider) {
+// Configure states for "home", "content" and "about"
+    $stateProvider.state('home', {
+        url:'/',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController',
+    })
+    .state('about', {
+        url:'/about',
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController',
+    })
+    .state('content', {
+        url:'/content',
+        templateUrl: 'templates/content.html',
+        controller: 'ContentController',
+    })
+});
+myApp.controller('HomeController', function($scope){
+    $scope.number = 0;
+})
+.controller('AboutController', function($scope){
+    $scope.about = "Good morning sleepy heads";
+})
+.controller('ContentController', function($scope){
+    $scope.url = "http://cdn.m3q.jp/upload/2014/11/7/l_2014-11-7_7b890881d49a84e9eeec801aacc3716a.jpg";
+})
